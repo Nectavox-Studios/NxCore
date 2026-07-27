d61224d6daa999d67c30c2afc80ba07be092a8c5
OZyroX <amirmahdi.momeni99@outlook.com>
2026-07-27 21:27:10 +0330

    change: Change Adventure Platform Version

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -4,7 +4,7 @@ version = rootProject.version
 dependencies {
     implementation(project(":api"))
     implementation("net.kyori:adventure-api:5.2.0")
-    implementation("net.kyori:adventure-platform-bukkit:4.3.4")
+    implementation("net.kyori:adventure-platform-bukkit:4.4.1")
     implementation("dev.triumphteam:triumph-gui:3.1.13")
 
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
